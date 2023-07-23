aecaafb9f750b611e2b963b136ed4043d6b8aacf
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-23 12:25:21 +0300

    Added localTime

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,16 @@ const store = (set, get) => ({
 	theme: 'dark',
 	setDarkTheme: () => set({ theme: 'dark' }),
 	setLightTheme: () => set({ theme: 'light' }),
+
+	localTime: '',
+	unpdateLocalTime: () => set({
+		localTime: new Intl.DateTimeFormat('en-US', {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			hour12: false,
+		}).format(new Date)
+	}),
 });
 
 export const useStore = create(store);
