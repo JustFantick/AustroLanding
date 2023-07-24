e3d9b5471be0a02d1fb2e1e977cfbad5b4e3fd17
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-24 11:14:46 +0300

    Created About section

---
+++ src/components/about/about.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import background from "../../img/map.png";
+
+export default function About() {
+	return (
+		<section className='about'
+			style={{
+				backgroundImage: `url(${background})`
+			}}
+		>
+			<div className="about__sub-column">
+				We are an innovative global real estate investment management company unlike any other.
+			</div>
+			<div className="about__main-column about-main-column">
+				<h2 className="about-main-column__title">what we do</h2>
+				<div className="about-main-column__description">
+					<p>We leverage our breadth of asset classes, vertically integrated services, unique business model, and in-house development and management expertise to deliver exceptional results, experiences and environments.</p>
+					<p>We craft high-quality, customized built environments that enhance the experiences of those who live and work in them, while driving value for our capital partners.</p>
+				</div>
+				<div className="about-main-column__numbers">
+					<div className="number-section"><span>17</span> years in business</div>
+					<div className="number-section"><span>124</span>projects</div>
+					<div className="number-section"><span>$160</span>total equity</div>
+				</div>
+
+
+			</div>
+
+		</section>
+	)
+}
