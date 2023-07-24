382168970a848702c480dee6457ff67c3e98809a
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-24 11:17:00 +0300

    Inserted About

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import StartScreen from './startScreen/startScreen.jsx';
 import { useStore } from '../store/store.js';
+import About from './about/about.jsx';
 
 export default function App() {
 	const theme = useStore(state => state.theme);
@@ -8,6 +9,7 @@ export default function App() {
 	return (
 		<div className={`wrapper ${theme}`}>
 			<StartScreen />
+			<About />
 		</div>
 	)
 }
