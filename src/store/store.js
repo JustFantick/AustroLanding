470d02eb2d13927d64172440ba7c6fc305d30613
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-26 19:10:48 +0300

    Added servicesList var and it`s setter

--- src/store/store.js
+++ src/store/store.js
@@ -14,6 +14,42 @@ const store = (set, get) => ({
 			hour12: false,
 		}).format(new Date)
 	}),
+
+	servicesList: [
+		{
+			chosen: true,
+			imgName: 'service1.png',
+			title: 'Overview',
+			text: 'Through our vertically integrated services, we own every phase of our work — from initial opportunities to ongoing management. From start to finish, we’re active, engaged, informed and accountable.',
+		},
+		{
+			chosen: false,
+			imgName: 'service1.png',
+			title: 'Investment management',
+			text: '1Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quidem quis officia nisi veniam nesciunt sapiente tempora libero aspernatur? Harum?',
+		},
+		{
+			chosen: false,
+			imgName: 'service1.png',
+			title: 'Development',
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis amet corrupti reprehenderit quo officiis sequi magnam corporis neque nam recusandae? Perspiciatis similique nam aspernatur voluptatem fugiat, sequi unde sint dignissimos?',
+		},
+		{
+			chosen: false,
+			imgName: 'service1.png',
+			title: 'Property management',
+			text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero aliquam nihil nulla vel blanditiis, debitis vero dolores hic praesentium provident adipisci temporibus harum dicta veritatis consequatur aspernatur nostrum minus assumenda?',
+		}
+	],
+	setServicesItemActive: (title) => set({
+		servicesList: get().servicesList.map(item => {
+			if (item.title !== title) {
+				return { ...item, chosen: false };
+			} else {
+				return { ...item, chosen: true };
+			}
+		})
+	})
 });
 
 export const useStore = create(store);
