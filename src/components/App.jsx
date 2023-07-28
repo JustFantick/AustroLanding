5b6d61df8f0e6d13c8c226d880b4e2cde79f5f28
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-28 10:25:15 +0300

    Implemented Services component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import StartScreen from './startScreen/startScreen.jsx';
 import { useStore } from '../store/store.js';
 import About from './about/about.jsx';
+import Services from './services/services.jsx';
 
 export default function App() {
 	const theme = useStore(state => state.theme);
@@ -10,6 +11,7 @@ export default function App() {
 		<div className={`wrapper ${theme}`}>
 			<StartScreen />
 			<About />
+			<Services />
 		</div>
 	)
 }
